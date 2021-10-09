2e7be94ac9d02d2781e40ba9688599865126a76b
Yichen Mo <amomorning@gmail.com>
2021-10-09 19:51:06 +0800

    Finish starting out

--- func.hs
+++ func.hs
@@ -4,3 +4,11 @@ dbu x y = db x + db y
 dbsmNum x = if x > 100 
     then x 
     else db x
+
+-- _ means anything from list
+length' xs = sum [1 | _ <- xs]   
+
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]   
+
+
